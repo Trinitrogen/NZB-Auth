bda67f023260589a90838790414d107c4f814dde
Kyle Bellgraph <kbellgra@gmail.com>
2020-04-03 11:39:45 -0500

    completed user table

--- nzb_auth.py
+++ nzb_auth.py
@@ -1,1 +1,6 @@
-from nzb_auth import app
+from nzb_auth import app, db
+from nzb_auth.models import User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
