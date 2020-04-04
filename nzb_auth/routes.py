38b0540ffe4bbd013bb99da8b887f325a449b14c
Kyle Bellgraph <kbellgra@gmail.com>
2020-04-04 11:30:43 -0500

    Completed login page and added nginx config

--- nzb_auth/routes.py
+++ nzb_auth/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, Response, url_for
 from nzb_auth import app
 from nzb_auth.forms import LoginForm
+from nzb_auth.models import User
+from flask_login import current_user, login_user
 
 
 @app.route('/')
@@ -21,18 +23,24 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        #return redirect(url_for('index'))
+        return Response(status=200)
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        print(f'Username: {form.username.data} Password: {form.password.data}')
-        return redirect(url_for(index))
-    #return Response("{'a':'b'}", status=401, mimetype='application/json')
-    #return render_template()
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        #return redirect(url_for('index'))
+        return Response(status=200)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/nzb_auth', methods=['GET', 'POST'])
 def nzb_auth():
     ''' Placeholder to check authentication. Automatically response 401'''
+    if current_user.is_authenticated:
+        return Response(status=200)
     return Response(status=401)
     #return Response("{'a':'b'}", status=401, mimetype='application/json')
